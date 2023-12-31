902e508a5f3a90e87be13dbced16a5461e762b0a
Mukul Sharma <mu.sharma@cgi.com>
2023-12-31 13:59:25 +0530

    import action

--- actions/test_host_get.py
+++ actions/test_host_get.py
@@ -1,10 +1,16 @@
 import requests
 import json
+from st2common.runners.base_action import Action
 
-class TestGetHost():
+class TestGetHost(Action):
 
     zabbix_url = "http://a03a00d009d6:8080/api_jsonrpc.php"
     api_key = "b62ffb555daf420447e17c7105b8307e85ee1962ab920692b8c5ec0e97b79dd7"
+
+    def __init__(self, config):
+        super(ZabbixBaseAction, self).__init__(config)
+
+        self.config = config
 
     def run(self, host_id=None):
         params = {"hostids": host_id}
