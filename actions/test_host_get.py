73985ab0491fa26640025d9fac3a5c88208ab02c
Mukul Sharma <mu.sharma@cgi.com>
2023-12-31 14:10:15 +0530

    changed params

--- actions/test_host_get.py
+++ actions/test_host_get.py
@@ -3,21 +3,16 @@ import json
 from st2common.runners.base_action import Action
 
 class TestGetHost(Action):
-
-    zabbix_url = "http://a03a00d009d6:8080/api_jsonrpc.php"
-    api_key = "b62ffb555daf420447e17c7105b8307e85ee1962ab920692b8c5ec0e97b79dd7"
-
     def __init__(self, config):
         super(TestGetHost, self).__init__(config)
-
         self.config = config
 
-    def run(self, host_id=None):
+    def run(self, zabbix_url, api_key, host_id):
         params = {"hostids": host_id}
-        data = {"jsonrpc": "2.0", "method": "host.get", "params": params, "id": 1, "auth": self.api_key}
+        data = {"jsonrpc": "2.0", "method": "host.get", "params": params, "id": 1, "auth": api_key}
 
         data = json.dumps(data)
 
-        output = requests.get(url=self.zabbix_url, data=data, headers={'Content-Type': 'application/json-rpc'} )
+        output = requests.get(url=zabbix_url, data=data, headers={'Content-Type': 'application/json-rpc'} )
 
         return output.json()
