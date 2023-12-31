290ff99066cadf8ec737c5ae750da9161f5312f8
Mukul Sharma <mu.sharma@cgi.com>
2023-12-31 13:46:09 +0530

    changes

---
+++ actions/test_host_get.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class TestGetHost():
+
+    zabbix_url = "http://a03a00d009d6:8080/api_jsonrpc.php"
+    api_key = "b62ffb555daf420447e17c7105b8307e85ee1962ab920692b8c5ec0e97b79dd7"
+
+    def __init__(self):
+        pass
+
+    def run(self, host_id=None):
+        params = {"hostids": host_id}
+        data = {"jsonrpc": "2.0", "method": "host.get", "params": params, "id": 1, "auth": self.api_key}
+
+        data = json.dumps(data)
+
+        output = requests.get(url=self.zabbix_url, data=data, headers={'Content-Type': 'application/json-rpc'} )
+
+        return output.json()
