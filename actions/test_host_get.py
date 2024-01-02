b7b4211508f0192f4d53a6f8b5349e56e6978d6b
Mukul Sharma <mu.sharma@cgi.com>
2024-01-02 21:35:56 +0530

    reverse changes

--- actions/test_host_get.py
+++ actions/test_host_get.py
@@ -7,18 +7,16 @@ class TestGetHost(Action):
         super().__init__(config)
         self.config = config
 
-    def run(self, customer_id, host_id):
+    def run(self, customer_id, host_id):   
+        for each in self.config['test_zabbix']:
+            if customer_id == each['customer_id']:
+                zabbix_url = each['zabbix_url']
+                api_key = each['api_key']
+
+        params = {"hostids": host_id}
+        data = {"jsonrpc": "2.0", "method": "host.get", "params": params, "id": 1, "auth": api_key}
+        data = json.dumps(data)
+
+        output = requests.get(url=zabbix_url, data=data, headers={'Content-Type': 'application/json-rpc'} )
         
-#        for each in self.config['test_zabbix']:
-#            if customer_id == each['customer_id']:
-#                zabbix_url = each['zabbix_url']
-#                api_key = each['api_key']
-#
-#        params = {"hostids": host_id}
-#        data = {"jsonrpc": "2.0", "method": "host.get", "params": params, "id": 1, "auth": api_key}
-
-#        data = json.dumps(data)
-
-#        output = requests.get(url=zabbix_url, data=data, headers={'Content-Type': 'application/json-rpc'} )
-#        return output.json()
-        return self.config
+        return output.json()
