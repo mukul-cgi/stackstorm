5435560c2ee54845ed7a32c1b361cb67f6d53a59
Mukul Sharma <mu.sharma@cgi.com>
2024-01-04 15:38:49 +0530

    separate req

--- actions/test_host_get.py
+++ actions/test_host_get.py
@@ -4,14 +4,24 @@ from lib.base import ZabbixBaseAction
 
 class TestGetHost(ZabbixBaseAction):
     def run(self, customer_id, host_id):   
-        customer_config = fetch_config(customer_id)
+        customer_config = super().fetch_config(customer_id)
         zabbix_url = customer_config['zabbix_url']
         api_key = customer_config['api_key']
 
         params = {"hostids": host_id}
         data = {"jsonrpc": "2.0", "method": "host.get", "params": params, "id": 1, "auth": api_key}
         data = json.dumps(data)
-
-        output = requests.get(url=zabbix_url, data=data, headers={'Content-Type': 'application/json-rpc'} )
         
-        return output.json()
+        return self.make_request(method="get", url=zabbix_url, data=data)
+    
+    def make_request(self, method, url, data):
+        headers={'Content-Type': 'application/json-rpc'}
+        response = requests.request(
+            method=method,
+            url=url,
+            data=data,
+            headers=headers,
+        )
+
+        response.json()
+
