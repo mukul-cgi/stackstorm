983f119a9df52664fdcbfcb0d01f3ef39ac316f7
Mukul Sharma <mu.sharma@cgi.com>
2024-01-05 16:43:14 +0530

    changed method

--- actions/test_host_get.py
+++ actions/test_host_get.py
@@ -3,16 +3,11 @@ from lib.base import ZabbixBaseAction
 class TestGetHost(ZabbixBaseAction):
     def run(self, customer_id, host_id):   
         self.fetch_config(customer_id)
-        #zabbix_url = customer_config['zabbix_url']
-        #api_key = customer_config['api_key']
 
         params = {"hostids": host_id}
-        #data = {"jsonrpc": "2.0", "method": "host.get", "params": params, "id": 1, "auth": api_key}
-    
-        #return self.make_request(method="get", url=zabbix_url, data=data)
-    
+
         return self.make_request(
-            http_method="get", 
+            http_method="post", 
             api_method="host.get", 
             params=params, 
             )
