52eaaaef759c0551601dc3723f2cb2a207448066
Mukul Sharma <mu.sharma@cgi.com>
2024-01-08 16:56:05 +0530

    test

--- actions/lib/base.py
+++ actions/lib/base.py
@@ -31,7 +31,7 @@ class ZabbixBaseAction(Action):
             headers=headers,
         )
         
-        return response
+        return response.json()
 #        if response is not None and response.ok:
 #            res = response.json()
 #            if isinstance(res, dict) and "result" in res:
