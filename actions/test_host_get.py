dcbb8d50f5fcca08ebf7f69f402611137642bb7b
Mukul Sharma <mu.sharma@cgi.com>
2024-01-12 17:36:50 +0530

    test

--- actions/test_host_get.py
+++ actions/test_host_get.py
@@ -4,11 +4,10 @@ class TestGetHost(ZabbixBaseAction):
     def run(self, customer_id, host_id, test):   
         self.fetch_config(customer_id)
 
-#        params = {"hostids": host_id}
+        params = {"hostids": host_id}
 
-#        return self.make_request(
-#            http_method="post", 
-#            api_method="host.get", 
-#            params=params, 
-#            )
-        return type(host_id)
+        return self.make_request(
+            http_method="post", 
+            api_method="host.get", 
+            params=params, 
+            )
