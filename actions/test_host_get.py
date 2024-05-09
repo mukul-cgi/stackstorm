f6ba832ba12b5e18d3ae80c107bab1589ee1f783
mukul-cgi <144820366+mukul-cgi@users.noreply.github.com>
2024-05-09 22:51:44 +0530

    Update test_host_get.py

--- actions/test_host_get.py
+++ actions/test_host_get.py
@@ -1,13 +1,26 @@
-from lib.base import ZabbixBaseAction
+import pika
+import sys
+from st2common.runners.base_action import Action
 
-class TestGetHost(ZabbixBaseAction):
-    def run(self, customer_id, host_id, test):   
-        obj =  self.fetch_config(customer_id)
-
-        params = {"hostids": host_id}
-
-        return obj.make_request(
-            http_method="post", 
-            api_method="host.get", 
-            params=params, 
-            )
+class TestGetHost(Action):
+    def run():   
+        credentials = pika.PlainCredentials("test", "test")
+        parameters = pika.ConnectionParameters('748959631f5b',
+                                           5672,
+                                           '/',
+                                           credentials)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+        channel.queue_declare(queue='task_queue1', durable=True)
+        
+        message = ' '.join(sys.argv[1:]) or "Hello World!"
+        
+        channel.basic_publish(
+            exchange='',
+            routing_key='task_queue1',
+            body=message,
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
+        connection.close()
+        print(" [x] Sent %r" % message)
+        
