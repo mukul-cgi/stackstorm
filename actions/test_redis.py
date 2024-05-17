f385cc9314ca1af2a211c4eb4d344eaaf18c4950
mukul-cgi <144820366+mukul-cgi@users.noreply.github.com>
2024-05-17 23:48:18 +0530

    Update test_redis.py

--- actions/test_redis.py
+++ actions/test_redis.py
@@ -1,10 +1,12 @@
 import redis
 from st2common.runners.base_action import Action
+from st2common.transport import utils as transport_utils
 
 class TestRedis(Action):
     def run(self):   
-        redis_client = redis.Redis(host='a0382acdcbda', port=6379, db=0)         
-        redis_client.set('key', 'value')
-        data = redis_client.get('key')
-        return data
+ #       redis_client = redis.Redis(host='a0382acdcbda', port=6379, db=0)         
+ #       redis_client.set('key', 'value')
+ #       data = redis_client.get('key')
+ 
+        return transport_utils.get_messaging_urls()
         
