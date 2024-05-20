2a23fca163b86998bb2763d547469584ce7a265f
mukul-cgi <144820366+mukul-cgi@users.noreply.github.com>
2024-05-20 23:29:36 +0530

    Update test_mq.py

--- actions/test_mq.py
+++ actions/test_mq.py
@@ -2,39 +2,15 @@ import pika
 import time
 from st2common.runners.base_action import Action
 
-import eventlet
 from kombu import Exchange
-
-from st2common import config
+#from st2common import config
 from st2common.transport.publishers import PoolPublisher
 
 class TestMq(Action):
-    def run(self):   
-#        credentials = pika.PlainCredentials("test", "test")
-#        parameters = pika.ConnectionParameters('748959631f5b',
-#                                           5672,
-#                                           '/',
-#                                           )
-#        connection = pika.BlockingConnection(parameters)
-#        channel = connection.channel()
-#        channel.queue_declare(queue='task_queue1', durable=True)
-        
-#        message = "Hello World!"
-        
-#        channel.basic_publish(
-#            exchange='',
-#            routing_key='task_queue1',
-#            body=message,
-#            properties=pika.BasicProperties(delivery_mode=2)
-#        )
-#        connection.close()
-#        time.sleep(20)
-#        return f"[x] Sent {message}"
+    def run(self, message):   
         exchange = Exchange('', type="direct")
-        
         publisher = PoolPublisher()
-        publisher.publish(payload='test_message', exchange=exchange, routing_key='task_queue1')
-        eventlet.sleep(0.5)
-        return "message sent"
+        a = publisher.publish(payload=message, exchange=exchange, routing_key='task_queue1')
+        return a, "message sent"
 
          
