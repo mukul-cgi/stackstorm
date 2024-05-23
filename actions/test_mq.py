21f7bf4e9369914721a550b805d753597e23c565
mukul-cgi <144820366+mukul-cgi@users.noreply.github.com>
2024-05-23 16:47:26 +0530

    Update test_mq.py

--- actions/test_mq.py
+++ actions/test_mq.py
@@ -2,15 +2,27 @@ import pika
 import time
 from st2common.runners.base_action import Action
 
-from kombu import Exchange
+from kombu import Connection, Exchange, Consumer, Queue
 #from st2common import config
 from st2common.transport.publishers import PoolPublisher
 
 class TestMq(Action):
+    def process_message(self, body, message):
+#        print("body is {body}")
+#        payload = {"message_body": "{body}"}
+#        self.sensor_service.dispatch(trigger="test.event2", payload=payload, trace_tag="tag123")
+        message.ack()
+        print(body)
+        return body
+    
     def run(self, message):   
         exchange = Exchange('', type="direct")
         publisher = PoolPublisher()
         a = publisher.publish(payload=message, exchange=exchange, routing_key='salt_jobs')
-        return a, "message sent"
-
-         
+        
+        rabbit_url = "amqp://guest:guest@rabbitmq:5672"
+        conn = Connection(rabbit_url)
+        exchange = Exchange("", type="direct")
+        queue = Queue(name="salt_finished", exchange=exchange, routing_key="salt_finished")
+        consumer = Consumer(conn, queues=queue, callbacks=[self.process_message], accept=['application/json', 'application/x-python-serialize', 'pickle'])
+        consumer.consume()
