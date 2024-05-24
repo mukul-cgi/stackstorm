b0a58526edcd2a682f78118841d8971033d5c8cf
mukul-cgi <144820366+mukul-cgi@users.noreply.github.com>
2024-05-24 15:22:37 +0530

    Update test_redis.py

--- actions/test_redis.py
+++ actions/test_redis.py
@@ -3,24 +3,20 @@ import os
 import requests
 from st2common.runners.base_action import Action
 from st2common.transport import utils as transport_utils
+from common import test
 
 class TestRedis(Action):
     def run(self):   
  #       redis_client = redis.Redis(host='a0382acdcbda', port=6379, db=0)         
  #       redis_client.set('key', 'value')
  #       data = redis_client.get('key')
-        key = os.environ['ST2_ACTION_AUTH_TOKEN']
-        base_url = os.environ['ST2_ACTION_API_URL']
-        api_url = f"{base_url}/traces/?trace_tag=tag123"
-        headers = {
-            'Accept': 'application/json',
-            'X-Auth-Token': key
-        }
-        ret = requests.get(url=api_url, headers=headers)
-        return ret.json()
-        
-
-        
-        
-        return key
+ #       key = os.environ['ST2_ACTION_AUTH_TOKEN']
+ #       base_url = os.environ['ST2_ACTION_API_URL']
+ #       api_url = f"{base_url}/traces/?trace_tag=tag123"
+ #       headers = {
+ #           'Accept': 'application/json',
+ #           'X-Auth-Token': key
+ #       }
+ #       ret = requests.get(url=api_url, headers=headers)
+        return test()
         
