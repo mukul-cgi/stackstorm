f75c8ccb0acce5723860987ba6b2a7986ec97714
mukul-cgi <144820366+mukul-cgi@users.noreply.github.com>
2024-05-27 21:16:57 +0530

    Update sensor1.py

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 import eventlet
-
+from datetime import datetime
 from st2reactor.sensor.base import Sensor
 import os 
 from random import randint
@@ -26,31 +26,38 @@ class HelloSensor(Sensor):
     def __init__(self, sensor_service, config):
         super(HelloSensor, self).__init__(sensor_service=sensor_service, config=config)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
+#    def setup(self):            
+#        rabbit_url = "amqp://guest:guest@rabbitmq:5672"
+#        self.conn = Connection(rabbit_url)
+#        exchange = Exchange("", type="direct")
+#        self.producer = Producer(exchange=exchange, channel=self.conn.channel(), routing_key='salt_finished')    
+#        finished_queue = Queue(name='salt_finished', exchange=exchange, routing_key='salt_finished')
+#        finished_queue.maybe_bind(self.conn)
+#        finished_queue.declare()
+#        self.queue = Queue(name="salt_jobs", exchange=exchange, routing_key="salt_jobs")
+#        self.consumer = Consumer(self.conn, queues=self.queue, callbacks=[self.process_message], accept=['application/json', 'application/x-python-serialize', 'pickle'])
+#        self.consumer.consume()
+
     def setup(self):
-        rabbit_url = "amqp://guest:guest@rabbitmq:5672"
-        self.conn = Connection(rabbit_url)
         exchange = Exchange("", type="direct")
-        self.producer = Producer(exchange=exchange, channel=self.conn.channel(), routing_key='salt_finished')    
-        finished_queue = Queue(name='salt_finished', exchange=exchange, routing_key='salt_finished')
-        finished_queue.maybe_bind(self.conn)
-        finished_queue.declare()
         self.queue = Queue(name="salt_jobs", exchange=exchange, routing_key="salt_jobs")
-        self.consumer = Consumer(self.conn, queues=self.queue, callbacks=[self.process_message], accept=['application/json', 'application/x-python-serialize', 'pickle'])
+        self.connection = transport_utils.get_connection():
+#        connection.connect()
+        self.consumer = Consumer(self.connection, queues=self.queue, callbacks=[self.process_message], accept=['application/json', 'application/x-python-serialize', 'pickle'])
         self.consumer.consume()
 
     def process_message(self, body, message):
         print("body is {body}")
 #        payload = {"message_body": "{body}"}
 #        self.sensor_service.dispatch(trigger="test.event2", payload=payload, trace_tag="tag123")
-        self.producer.publish(body)
-        self._logger.warning(f"message body - {body}")
+#        self.producer.publish(body)
+#        self._logger.warning(f"message body - {body}")
         message.ack()
 
     def run(self):
         while True:
-
-            self.conn.drain_events()
-            eventlet.sleep(10)
+            self.connection.drain_events()
+            eventlet.sleep(20)
 
     def cleanup(self):
         pass
